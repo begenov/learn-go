e50189108cceb9d5bb99abf0d741b54015c4de6c
orazali <orazali@noreply.example.org>
2022-08-08 07:54:45 +0000

    Upload files to ''

---
+++ iterativepower.go
@@ -0,0 +1,15 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	if nb < 0 {
+		return 0
+	} else if 1 == power {
+		return 1
+	} else {
+		res := 1
+		for i := 0; i < power; i++ {
+			res *= nb
+		}
+		return res
+	}
+}
