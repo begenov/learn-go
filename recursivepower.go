d8027d40439642dc300d43e5337e211b62242177
orazali <orazali@noreply.example.org>
2022-08-08 10:03:18 +0000

    Upload files to ''

--- recursivepower.go
+++ recursivepower.go
@@ -2,10 +2,11 @@ package piscine
 
 func RecursivePower(nb int, power int) int {
 	if power < 0 {
-		return 0
-	} else if power == 0 {
+		return 0s
+	} else if power == 0 || power == 1 {
 		return 1
 	} else {
 		return nb * RecursivePower(nb, power-1)
 	}
 }
+
