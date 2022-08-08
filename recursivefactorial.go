9949ebb845693cb7cd402a79d7ba11c3a4297d4b
orazali <orazali@noreply.example.org>
2022-08-08 07:25:29 +0000

    Upload files to ''

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,15 +1,17 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb < 0 {
-		return 0
-	} else if nb == 0 {
+	if nb == 1 {
 		return 1
+	} else if nb < 0 {
+		return 0
 	}
 
 	fac := nb * RecursiveFactorial(nb-1)
 	if fac < 0 {
 		return 0
+	} else if fac == 0 {
+		return fac
 	}
 	return fac
 }
