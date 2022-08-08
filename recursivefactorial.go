ff6384cbd71347fa4a9a1e1128949c52c739f2ab
orazali <orazali@noreply.example.org>
2022-08-08 06:12:22 +0000

    Upload files to ''

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,8 +1,19 @@
-package piscine
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arg := 4
+	fmt.Println(RecursiveFactorial(arg))
+}
 
 func RecursiveFactorial(nb int) int {
 	if nb == 0 {
 		return 1
+	} else if nb < 0 {
+		return 0
 	}
 	return nb * RecursiveFactorial(nb-1)
 }
