7318a5d33fbe0348d61ae2f938d852361fb8cf47
orazali <orazali@noreply.example.org>
2022-08-08 09:31:36 +0000

    Upload files to ''

--- fibonacci.go
+++ fibonacci.go
@@ -3,7 +3,9 @@ package piscine
 func Fibonacci(index int) int {
 	if index < 0 {
 		return -1
-	} else if index == 1 || index == 0 {
+	} else if index == 1 {
+		return 1
+	} else if index == 0 {
 		return 0
 	}
 	return Fibonacci(index-1) + Fibonacci(index-2)
