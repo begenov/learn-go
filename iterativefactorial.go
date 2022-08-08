dd6f1f27b5dc9a32e312d41f7b574afd7fd79028
orazali <orazali@noreply.example.org>
2022-08-08 06:02:37 +0000

    Upload files to ''

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,14 +1,17 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb < 0 || nb > 12 {
+	if nb < 0 {
 		return 0
 	} else if nb == 0 {
 		return 1
 	}
-	a := nb - 1
-	for i := a; i >= 1; i-- {
+	count := nb - 1
+	for i := count; i >= 1; i-- {
 		nb *= i
+		if nb < 0 {
+			return 0
+		}
 	}
 	return nb
 }
