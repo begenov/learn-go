dcbb7dff381996eab945cda48033fb9714fa93cd
oraazali <begenovorazali@icloud.com>
2022-08-09 10:59:44 +0600

    asf

---
+++ sqrt.go
@@ -0,0 +1,19 @@
+package main
+
+func Sqrt(nb int) int  {
+	if nb <= 0 {
+		return 0
+	} else if nb == 1 {
+		return 1
+	}
+	count := 0
+	for i := 0; i < nb; i++ {
+		count = i * i
+		if count > nb {
+			return 0
+		} else if nb == count {
+			return i
+		}
+	}
+	return 0
+}
