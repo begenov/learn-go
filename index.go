b939bf93d0e07b346294d1480376d2e31654a6bc
oraazali <begenovorazali@icloud.com>
2022-08-09 20:08:30 +0600

    asfg

--- index.go
+++ index.go
@@ -1,16 +1,16 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	if len(s) == 0{
+	if len(s) == 0 {
 		return 0
-	} else if len(s) == 1{
+	} else if len(s) == 1 {
 		return 1
 	}
 	a := 0
 	for i := range s {
 		if s[i] == toFind[a] {
-			if a == len(toFind) - 1 {
-				return i - (len(toFind) -1)
+			if a == len(toFind)-1 {
+				return i - (len(toFind) - 1)
 			}
 			a++
 		}
