13eb49b92234a80079a6f49bd63611fd926234e5
oraazali <begenovorazali@icloud.com>
2022-08-09 18:28:11 +0600

    asf

---
+++ alphacount.go
@@ -0,0 +1,13 @@
+package piscine
+
+func AlphaCount(s string) int {
+	a := []rune(s)
+
+	count := 0
+	for _,  value := range a {
+		if value >= 'A' && value <= 'Z' || value >= 'a' && value <= 'z' {
+			count++
+		}
+	}
+	return count
+}
