96a577d9b9cd7addc03512b33304b4a883854dd5
oraazali <begenovorazali@icloud.com>
2022-08-09 12:21:05 +0600

    asfas

--- isprime.go
+++ isprime.go
@@ -1,14 +1,13 @@
 package piscine
 
 func IsPrime(nb int) bool {
-
 	if nb < 0 || nb == 0 || nb == 1 {
 		return false
-	} else if  nb == 2 || nb == 3 {
+	} else if nb == 2 || nb == 3 {
 		return true
 	}
 	for i := 2; i < nb; i++ {
-		count := nb%i
+		count := nb % i
 		if count == 0 {
 			return false
 		}
