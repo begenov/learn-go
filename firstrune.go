46d43739a8002cfbc9cbeee423aaf3fad7b3abea
oraazali <begenovorazali@icloud.com>
2022-08-09 14:15:33 +0600

    asffg

--- firstrune.go
+++ firstrune.go
@@ -1,16 +1,5 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
-
-func main() {
-	z01.PrintRune(FirstRune("Hello!"))
-	z01.PrintRune(FirstRune("Salut!"))
-	z01.PrintRune(FirstRune("Ola!"))
-	z01.PrintRune('\n')
-}
-
 func FirstRune(s string) rune {
 	a := []rune(s)
 	return a[0]
