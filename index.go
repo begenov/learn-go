8a876c80ad633839161755895cc0d50bc4ce0a4f
oraazali <begenovorazali@icloud.com>
2022-08-09 20:38:33 +0600

    asfg

--- index.go
+++ index.go
@@ -1,10 +1,19 @@
-package piscine
+package main
+
+import (
+	"fmt"
+)
+func main() {
+	fmt.Println(Index("Hello!", "l"))
+	fmt.Println(Index("Salut!", "alu"))
+	fmt.Println(Index("Ola!", "hOl"))
+}
 
 func Index(s string, toFind string) int {
 	if len(s) == 0 {
 		return 0
 	} else if len(s) == 1 {
-		return 1
+		return 0
 	}
 	a := 0
 	for i := range s {
