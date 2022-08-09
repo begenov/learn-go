c0d317157c749a46ea764960ed0b7a67bd0d22ea
oraazali <begenovorazali@icloud.com>
2022-08-09 20:08:04 +0600

    asfg

---
+++ index.go
@@ -0,0 +1,19 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	if len(s) == 0{
+		return 0
+	} else if len(s) == 1{
+		return 1
+	}
+	a := 0
+	for i := range s {
+		if s[i] == toFind[a] {
+			if a == len(toFind) - 1 {
+				return i - (len(toFind) -1)
+			}
+			a++
+		}
+	}
+	return 0
+}
