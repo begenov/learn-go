72cc9ab686a47622afaeb97ae245fd862a78eade
oraazali <begenovorazali@icloud.com>
2022-08-09 18:28:53 +0600

    asf

--- alphacount.go
+++ alphacount.go
@@ -4,7 +4,7 @@ func AlphaCount(s string) int {
 	a := []rune(s)
 
 	count := 0
-	for _,  value := range a {
+	for _, value := range a {
 		if value >= 'A' && value <= 'Z' || value >= 'a' && value <= 'z' {
 			count++
 		}
