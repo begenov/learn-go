c96c838e1a963e93e1316c95b8eaf24b68ff3305
oraazali <begenovorazali@icloud.com>
2022-08-09 14:02:06 +0600

    asffg

---
+++ firstrune.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	z01.PrintRune(FirstRune("Hello!"))
+	z01.PrintRune(FirstRune("Salut!"))
+	z01.PrintRune(FirstRune("Ola!"))
+	z01.PrintRune('\n')
+}
+func FirstRune(s string) rune {
+	a := []rune(s)
+	return a[0]
+}
+
