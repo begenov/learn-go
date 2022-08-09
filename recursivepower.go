676e006f17c17bd24e1963c28255034382571b89
oraazali <begenovorazali@icloud.com>
2022-08-09 12:22:51 +0600

    asf

--- recursivepower.go
+++ recursivepower.go
@@ -3,7 +3,9 @@ package piscine
 func RecursivePower(nb int, power int) int {
 	if power < 0 {
 		return 0
-	} else if power == 0 || power == 1 || nb == 0 {
+	} else if 1 == power {
+		return nb
+	} else if 0 == power {
 		return 1
 	} else {
 		return nb * RecursivePower(nb, power-1)
