07fa93cd84e855c7bedb588e3c13c3a029841ba6
oraazali <begenovorazali@icloud.com>
2022-08-09 11:56:01 +0600

    formattinga

---
+++ isprime.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.IsPrime(5))
+	fmt.Println(piscine.IsPrime(4))
+}
--- recursivepower.go
+++ recursivepower.go
@@ -1,9 +1,9 @@
 package piscine
 
 func RecursivePower(nb int, power int) int {
-	if power < 0 ||{
+	if power < 0 {
 		return 0
-	} else if power == 0 || power == 1 || nb == 0{
+	} else if power == 0 || power == 1 || nb == 0 {
 		return 1
 	} else {
 		return nb * RecursivePower(nb, power-1)
