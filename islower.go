5150637dfae17a142315a92d659da454d55d296a
orazali <begenovorazali@icloud.com>
2022-08-10 13:15:20 +0600

    asfg

--- islower.go
+++ islower.go
@@ -1,10 +1,11 @@
 package piscine
 
 func IsLower(s string) bool {
+	count := 0
 	for _, i := range s {
-		if i <= 'a' || i >= 'z' {
-			return false
+		if i >= 'a' && i <= 'z' {
+			count++
 		}
 	}
-	return true
+	return count == len(s)
 }
