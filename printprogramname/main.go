f842f491b0911c8ce6024acb9ce0fd5303067661
orazali <begenovorazali@icloud.com>
2022-08-11 12:04:11 +0600

    asf

---
+++ printprogramname/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arguments := os.Args
+
+	for _, i := range arguments[0][2:] {
+		z01.PrintRune(i)
+	}
+	z01.PrintRune('\n')
+}
