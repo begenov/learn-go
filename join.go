26368a9177c923e9ff6eef2c31f3c45a3bed8d0d
Your Name <you@example.com>
2022-08-11 19:10:42 +0600

    asgf

---
+++ join.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Join(strs []string, sep string) string {
+	count := ""
+	sep_s := ":"
+	for index, i := range strs {
+		count += i
+		if index != len(strs)-1 {
+			count += sep_s
+		}
+	}
+	return count
+}
