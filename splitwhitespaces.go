cebdaf2c261d8b955407720b146e739ba2f72a11
Your Name <you@example.com>
2022-08-13 20:42:00 +0600

    asf

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -17,7 +17,10 @@ func SplitWhiteSpaces(s string) []string {
 			b = true
 		}
 	}
-
-	an = append(an, af)
+	if len(an) <= len(as) {
+		an = append(an, af)
+	}
 	return an
 }
+
+// SplitWhiteSpaces("RSWj<hjS3)IiI Q7qMJtCUy\"LtF c38?%F5hf\\6Q| <Ki: %V/_C-ET 718SLx7ofj.]\" (=\"If7K8IRL26 \\[/Nv0_c/ER>\" 67^?oO_DwOAz ") == []string{"RSWj<hjS3)IiI", "Q7qMJtCUy\"LtF", "c38?%F5hf\\6Q|", "<Ki:", "%V/_C-ET", "718SLx7ofj.]\"", "(=\"If7K8IRL26", "\\[/Nv0_c/ER>\"", "67^?oO_DwOAz", ""} instead of []string{"RSWj<hjS3)IiI", "Q7qMJtCUy\"LtF", "c38?%F5hf\\6Q|", "<Ki:", "%V/_C-ET", "718SLx7ofj.]\"", "(=\"If7K8IRL26", "\\[/Nv0_c/ER>\"", "67^?oO_DwOAz"}
