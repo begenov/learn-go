5151f814d1ac5d9beb91d0c01e01391d6707ef13
Your Name <you@example.com>
2022-08-18 15:43:49 +0600

    quest3

--- atoi.go
+++ atoi.go
@@ -1,19 +1,19 @@
-package main
+package piscine
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-func main() {
-	fmt.Println(Atoi("12345"))
-	fmt.Println(Atoi("0000000012345"))
-	fmt.Println(Atoi("012 345"))
-	fmt.Println(Atoi("Hello World!"))
-	fmt.Println(Atoi("+1234"))
-	fmt.Println(Atoi("-1234"))
-	fmt.Println(Atoi("++1234"))
-	fmt.Println(Atoi("--1234"))
-}
+// func main() {
+// 	fmt.Println(Atoi("12345"))
+// 	fmt.Println(Atoi("0000000012345"))
+// 	fmt.Println(Atoi("012 345"))
+// 	fmt.Println(Atoi("Hello World!"))
+// 	fmt.Println(Atoi("+1234"))
+// 	fmt.Println(Atoi("-1234"))
+// 	fmt.Println(Atoi("++1234"))
+// 	fmt.Println(Atoi("--1234"))
+// }
 
 // func Atoi(s string) int {
 // 	count := 0
