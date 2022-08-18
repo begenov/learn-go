572307eb2fffab8669197fd87bfe0c4e021c90b4
Your Name <you@example.com>
2022-08-18 18:15:28 +0600

    hackaton

--- enigma.go
+++ enigma.go
@@ -1,54 +1,12 @@
-package main
-
-import (
-	"fmt"
-)
-
-func main() {
-	x := 5
-	y := &x
-	z := &y
-	a := &z
-
-	w := 2
-	b := &w
-
-	u := 7
-	e := &u
-	f := &e
-	g := &f
-	h := &g
-	i := &h
-	j := &i
-	c := &j
-
-	k := 6
-	l := &k
-	m := &l
-	n := &m
-	d := &n
-
-	fmt.Println(***a)
-	fmt.Println(*b)
-	fmt.Println(*******c)
-	fmt.Println(****d)
-
-	Enigma(a, b, c, d)
-
-	fmt.Println("After using Enigma")
-	fmt.Println(***a)
-	fmt.Println(*b)
-	fmt.Println(*******c)
-	fmt.Println(****d)
-}
+package piscine
 
 func Enigma(a ***int, b *int, c *******int, d ****int) {
-	a1 := ***a
-	b1 := *b
-	c1 := *******c
-	d1 := ****d
-	***a = c1
-	*******c = d1
-	****d = b1
-	*b = a1
+	a1 := ***a     // 5
+	b1 := *b       // 2
+	c1 := *******c // 7
+	d1 := ****d    // 6
+	***a = b1
+	*b = d1
+	*******c = a1
+	****d = c1
 }
