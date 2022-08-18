e9428c758f0d1fa90dc4d54b1dd489534f3a7ded
Your Name <you@example.com>
2022-08-18 20:07:08 +0600

    hackaton

---
+++ pilot/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Pilot struct {
+	Name     string
+	Life     float64
+	Age      int
+	Aircraft string
+}
+
+func main() {
+	var donnie Pilot
+	donnie.Name = "Donnie"
+	donnie.Life = 100.0
+	donnie.Age = 24
+	donnie.Aircraft = "AIRCRAFT1"
+	fmt.Println(donnie)
+}
+
+const AIRCRAFT1 = 1
