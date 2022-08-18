db40c10ecc2f4e872f193830d19bf3a79f531d2e
Your Name <you@example.com>
2022-08-18 15:07:08 +0600

    hackaton

---
+++ comcheck/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	arg := os.Args[1:]
+	for i := range arg {
+		if arg[i] == "galaxy" || arg[i] == "galaxy 01" {
+			fmt.Println("Alert!!!")
+		}
+	}
+}
