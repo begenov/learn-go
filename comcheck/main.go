3667513ab127afd2d8c1e58f0e4280140dfe05d9
Your Name <you@example.com>
2022-08-18 15:07:48 +0600

    hackaton

--- comcheck/main.go
+++ comcheck/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	arg := os.Args[1:]
 	for i := range arg {
-		if arg[i] == "galaxy" || arg[i] == "galaxy 01" {
+		if arg[i] == "galaxy" || arg[i] == "galaxy 01" || arg[i] == "01" {
 			fmt.Println("Alert!!!")
 		}
 	}
