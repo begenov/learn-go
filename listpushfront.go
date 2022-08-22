7454891a446866d290a2b7535381b0476ecaaec1
Your Name <you@example.com>
2022-08-22 16:16:08 +0600

    q1

--- listpushfront.go
+++ listpushfront.go
@@ -1,15 +1,5 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
 func ListPushFront(l *List, data interface{}) {
 	if l.Head == nil {
 		l.Head, l.Tail = &NodeL{Data: data}, l.Head
