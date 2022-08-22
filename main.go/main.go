b800e09a0bbf816631c1f5d91b1549f371ae3df0
Your Name <you@example.com>
2022-08-22 13:29:26 +0600

    as

---
+++ main.go/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	link := &List{}
+
+	ListPushBack(link, "Hello")
+	ListPushBack(link, "man")
+	ListPushBack(link, "how are you")
+
+	for link.Head != nil {
+		fmt.Println(link.Head.Data)
+		link.Head = link.Head.Next
+	}
+}
