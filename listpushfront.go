27de26d6c748493cfc3473fbad0fea9642a38b0c
Your Name <you@example.com>
2022-08-23 14:57:58 +0600

    assag

--- listpushfront.go
+++ listpushfront.go
@@ -42,15 +42,15 @@ package piscine
 // }
 // package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
+// type NodeL struct {
+// 	Data interface{}
+// 	Next *NodeL
+// }
 
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
+// type List struct {
+// 	Head *NodeL
+// 	Tail *NodeL
+// }
 
 func ListPushFront(l *List, data interface{}) {
 	if l.Head == nil {
