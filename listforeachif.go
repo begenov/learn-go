8577612cc50f859f3369144d4d368f579f1c1b14
Your Name <you@example.com>
2022-08-23 11:52:37 +0600

    ffs

--- listforeachif.go
+++ listforeachif.go
@@ -1,92 +1,14 @@
 package piscine
 
-// package main
-
-// import (
-// 	"fmt"
-// )
-
-// func PrintElem(node *NodeL) {
-// 	fmt.Println(node.Data)
+// type NodeL struct {
+// 	Data interface{}
+// 	Next *NodeL
 // }
 
-// func StringToInt(node *NodeL) {
-// 	node.Data = 2
+// type List struct {
+// 	Head *NodeL
+// 	Tail *NodeL
 // }
-
-// func PrintList(l *List) {
-// 	it := l.Head
-// 	for it != nil {
-// 		fmt.Print(it.Data, "->")
-// 		it = it.Next
-// 	}
-// 	fmt.Print("nil", "\n")
-// }
-
-// func main() {
-// 	link := &List{}
-
-// 	ListPushBack(link, 1)
-// 	ListPushBack(link, "hello")
-// 	ListPushBack(link, 3)
-// 	ListPushBack(link, "there")
-// 	ListPushBack(link, 23)
-// 	ListPushBack(link, "!")
-// 	ListPushBack(link, 54)
-
-// 	PrintList(link)
-
-// 	fmt.Println("--------function applied--------")
-// 	ListForEachIf(link, PrintElem, IsPositiveNode)
-
-// 	ListForEachIf(link, StringToInt, IsAlNode)
-
-// 	fmt.Println("--------function applied--------")
-// 	PrintList(link)
-
-// 	fmt.Println()
-// }
-
-func IsNumeric_node(node *NodeL) bool {
-	switch node.Data.(type) {
-	case int, float32, float64, byte:
-		return true
-	case string, rune:
-		return false
-	}
-	return false
-}
-
-func ListForEachIf(l *List, f func(*NodeL), comp func(*NodeL) bool) {
-	it := l.Head
-	for it != nil {
-		if comp(it) {
-			f(it)
-		}
-		it = it.Next
-	}
-}
-
-// func ListPushBack(l *List, data interface{}) {
-// 	n := &NodeL{Data: data}
-// 	if l.Head == nil {
-// 		l.Head = n
-// 		l.Tail = n
-// 	} else {
-// 		l.Tail.Next = n
-// 		l.Tail = n
-// 	}
-// }
-
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
 
 func IsPositiveNode(node *NodeL) bool {
 	switch node.Data.(type) {
@@ -103,5 +25,15 @@ func IsAlNode(node *NodeL) bool {
 		return false
 	default:
 		return true
+	}
+}
+
+func ListForEachIf(l *List, f func(*NodeL), comp func(*NodeL) bool) {
+	it := l.Head
+	for it != nil {
+		if comp(it) {
+			f(it)
+		}
+		it = it.Next
 	}
 }
