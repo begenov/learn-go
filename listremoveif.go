be15b87aef7b1ad0ffcbfdb38bd07706599be81a
Your Name <you@example.com>
2022-08-23 14:22:50 +0600

    asf

--- listremoveif.go
+++ listremoveif.go
@@ -6,17 +6,18 @@ func ListRemoveIf(l *List, data_ref interface{}) {
 	}
 	current := l.Head
 	var previous *NodeL
- 
+
 	for current != nil {
 		if current.Data == data_ref {
 			if previous == nil {
-				l.Head = current.Next 	
-			}else{
+				l.Head = current.Next
+			} else {
 				previous.Next = current.Next
 				current = previous
 			}
-		}else{
+		} else {
 			previous = current
 		}
-		current = current.Next			
+		current = current.Next
 	}
+}
