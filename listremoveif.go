a54d2a64f4002fa2ea8259875ffeb8eb4db4659e
Your Name <you@example.com>
2022-08-23 14:42:18 +0600

    asfg

--- listremoveif.go
+++ listremoveif.go
@@ -1,23 +1,53 @@
 package piscine
 
-func ListRemoveIf(l *List, data_ref interface{}) {
-	if l == nil {
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func PrintList(l List) {
+// 	it := l.Head
+// 	for it != nil {
+// 		fmt.Print(it.Data, " -> ")
+// 		it = it.Next
+// 	}
+// 	fmt.Print(nil, "\n")
+// }
+
+// func main() {
+// 	link := List{}
+// 	link2 := List{}
+
+// 	ListPushBack(link, "a")
+// 	ListPushBack(link, "b")
+// 	ListPushBack(link, "c")
+// 	ListPushBack(link, "d")
+// 	fmt.Println("-----first List------")
+// 	PrintList(link)
+
+// 	ListPushBack(link2, "e")
+// 	ListPushBack(link2, "f")
+// 	ListPushBack(link2, "g")
+// 	ListPushBack(link2, "h")
+// 	fmt.Println("-----second List------")
+// 	PrintList(link2)
+
+// 	fmt.Println("-----Merged List-----")
+// 	ListMerge(link, link2)
+// 	PrintList(link)
+// }
+
+func ListMerge(l1 *List, l2 *List) {
+	if l2 == nil || l1 == nil {
 		return
 	}
-	current := l.Head
-	var previous *NodeL
 
-	for current != nil {
-		if current.Data == data_ref {
-			if previous == nil {
-				l.Head = current.Next
-			} else {
-				previous.Next = current.Next
-				current = previous
-			}
-		} else {
-			previous = current
-		}
-		current = current.Next
+	if l1.Head == nil {
+		l1.Head = l2.Head
+		l1.Tail = l2.Head
+		return
 	}
+
+	l1.Tail.Next = l2.Head
 }
--- listsort.go
+++ listsort.go
@@ -1,13 +1,49 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
+// package main
 
-type List struct {
-	Head *NodeL
-	Tail *NodeL
+// import (
+// 	"fmt"
+// )
+
+// func PrintList(l *NodeI) {
+// 	it := l
+// 	for it != nil {
+// 		fmt.Print(it.Data, " -> ")
+// 		it = it.Next
+// 	}
+// 	fmt.Print(nil, "\n")
+// }
+
+// func listPushBack(l *NodeI, data int) *NodeI {
+// 	n := &NodeI{Data: data}
+
+// 	if l == nil {
+// 		return n
+// 	}
+// 	iterator := l
+// 	for iterator.Next != nil {
+// 		iterator = iterator.Next
+// 	}
+// 	iterator.Next = n
+// 	return l
+// }
+
+// func main() {
+// 	var link *NodeI
+
+// 	link = listPushBack(link, 5)
+// 	link = listPushBack(link, 4)
+// 	link = listPushBack(link, 3)
+// 	link = listPushBack(link, 2)
+// 	link = listPushBack(link, 1)
+
+// 	PrintList(ListSort(link))
+// }
+
+type NodeI struct {
+	Data int
+	Next *NodeI
 }
 
 func ListSort(l *NodeI) *NodeI {
