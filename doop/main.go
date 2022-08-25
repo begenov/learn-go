dc1ff5ff054c5d1714fb1d87fb538a53aad348e0
Your Name <you@example.com>
2022-08-25 14:21:37 +0600

    doop

---
+++ doop/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func validateOperator(test string) bool {
+	op := []string{"+", "-", "*", "/", "%"}
+	for _, res := range op {
+		if res == test {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	args := os.Args[1:]
+	if len(args) > 3 || len(args) < 3 {
+		fmt.Print()
+	} else {
+		if !validateOperator(args[1]) {
+			fmt.Println(0)
+		} else {
+			premier, _ := strconv.Atoi(args[0])
+			second, _ := strconv.Atoi(args[2])
+
+			if args[1] == "%" && second == 0 {
+				fmt.Print("No Modulo by 0\n")
+			} else if args[1] == "/" && second == 0 {
+				fmt.Print("No division by 0\n")
+			} else if args[1] == "+" {
+				fmt.Println(premier + second)
+			} else if args[1] == "-" {
+				fmt.Println(premier - second)
+			} else if args[1] == "*" {
+				fmt.Println(premier * second)
+			} else if args[1] == "/" {
+				fmt.Println(premier / second)
+			} else {
+				fmt.Println(premier % second)
+			}
+
+		}
+	}
+}
