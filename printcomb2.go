86849ab2c944a98679335fb4d2ace4fbf213fabc
Your Name <you@example.com>
2022-08-25 14:00:36 +0600

    quest2

--- printcomb2.go
+++ printcomb2.go
@@ -2,25 +2,37 @@ package piscine
 
 import "github.com/01-edu/z01"
 
+// package main
+
+// import (
+// 	"github.com/01-edu/z01"
+// )
+
+// func main() {
+// 	PrintComb2()
+// }
+
 func PrintComb2() {
-	for num := 0; num <= 98; num++ {
-		for iw := num + 1; iw <= 99; iw++ {
-			a := 48 + (num / 10)
-			b := 48 + (num % 10)
-			c := 48 + (iw / 10)
-			d := 48 + (iw % 10)
-			if num <= iw {
-				z01.PrintRune(rune(a))
-				z01.PrintRune(rune(b))
+	for i := 0; i <= 98; i++ {
+		for j := i + 1; j <= 99; j++ {
+			x := 48 + (i / 10)
+			y := 48 + (i % 10)
+			z := 48 + (j / 10)
+			d := 48 + (j % 10)
+			if i <= j {
+				z01.PrintRune(rune(x))
+				z01.PrintRune(rune(y))
 				z01.PrintRune(rune(' '))
-				z01.PrintRune(rune(c))
+
+				z01.PrintRune(rune(z))
 				z01.PrintRune(rune(d))
+
 			}
-			if num != 98 {
-				z01.PrintRune(rune(','))
-				z01.PrintRune(rune(' '))
+			if i != 98 {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
 			} else {
-				z01.PrintRune(rune('\n'))
+				z01.PrintRune('\n')
 			}
 		}
 	}
