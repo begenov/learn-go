f6ad5c1b874250c05be85d8543a3aa3e94907e55
Your Name <you@example.com>
2022-10-23 20:06:35 +0600

    asd

--- doop/main.go
+++ doop/main.go
@@ -5,81 +5,60 @@ import (
 	"os"
 )
 
-func operations(num1 int, num2 int, oper string) {
-	result := 0
-	if oper == "+" || oper == "\"+\"" {
-		result = num1 + num2
-		if num1 == 9223372036854775807 {
-			return
-		}
-		fmt.Println(result)
+func main() {
+	args := os.Args[1:]
+	if len(args) != 3 {
+		return
 	}
-	if oper == "-" || oper == "\"-\"" {
-		result = num1 - num2
-		if num1 == -9223372036854775807 {
-			return
-		}
-		fmt.Println(result)
-	}
-	if oper == "/" || oper == "\"/\"" {
-		if num2 == 0 {
-			fmt.Println("No division by 0")
-			return
-		}
-		result = num1 / num2
-		fmt.Println(result)
-	}
-	if oper == "*" || oper == "\"*\"" {
-		result = num1 * num2
-		if num1 == 9223372036854775807 {
-			return
-		}
-		fmt.Println(result)
-	}
-	if oper == "%" || oper == "\"%\"" {
-		if num2 == 0 {
-			fmt.Println("No modulo by 0")
-			return
-		}
-		result = num1 % num2
-		fmt.Println(result)
+	n1 := Atoi(args[0])
+	n2 := Atoi(args[2])
+	ar := args[1]
+	switch ar {
+	case "+":
+		fmt.Println(n1 + n2)
+	case "-":
+		fmt.Println(n1 - n2)
+	case "*":
+		fmt.Println(n1 * n2)
+	case "/":
+		fmt.Println(n1 / n2)
+	case "%":
+		fmt.Println(n1 % n2)
 	}
 }
 
-func main() {
-	args := os.Args[1:]
-	num1 := 0
-	num2 := 0
-	u := false
-	o := false
-	if len(args) == 3 {
-		for _, arg1 := range args[0] {
-			if arg1 == 45 {
-				u = true
-				continue
-			}
-			if (arg1 < '0' || arg1 > '9') && arg1 != 45 {
-				return
-			}
-			num1 = (num1*10 + int((arg1 - 48)))
-		}
-		if u {
-			num1 *= -1
-		}
-		for _, arg2 := range args[2] {
-			if arg2 == 45 {
-				o = true
-				continue
-			}
-			if (arg2 < '0' || arg2 > '9') && arg2 != 45 {
-				return
-			}
-			num2 = (num2*10 + int((arg2 - 48)))
-		}
-		if o {
-			num2 *= -1
-		}
-		operations(num1, num2, args[1])
-
+func Atoi(s string) int {
+	flag := false
+	if s[0] == '-' {
+		flag = true
+		s = s[1:]
 	}
+	res := 0
+	for i := 0; i < len(s); i++ {
+		// if s[i] == '-' {
+		// 	continue
+		// }
+		res = res*10 + int(rune(s[i]-48))
+	}
+	if flag {
+		res *= -1
+	}
+	return res
+}
+
+func Itoa(n int) string {
+	flag := false
+	if n < 0 {
+		flag = true
+		n = n * -1
+	}
+	res := ""
+	for n > 0 {
+		res = string(rune(n%10)+48) + res
+		n = n / 10
+	}
+	if flag {
+		res = "-" + res
+	}
+	return res
 }
